da90d826916a7a164d5da6ce266acc1f7541f68c
dboyliao <qmalliao@gmail.com>
2021-05-08 14:50:10 +0800

    update script

--- Python/pyscript-run.py
+++ Python/pyscript-run.py
@@ -5,16 +5,27 @@ from pathlib import Path
 import sys
 import os
 import subprocess
-from shutil import copyfile
+from shutil import copyfile, rmtree, copy
 
 
-def pyscript_run(py_script, script_args):
+_LOG_DIR = "~/.pyscript_run"
+
+
+def clear(py_script: str):
+    print(f"clear log for {py_script}")
+    log_path = Path(_LOG_DIR) / py_script
+    if log_path.exists():
+        rmtree(str(log_path))
+    return 0
+
+
+def pyscript_run(py_script, script_args, copy_dir=False):
     run_time = dt.datetime.now().isoformat(timespec="seconds").replace(":", "-")
     script_name, ext = os.path.splitext(py_script)
     if ext != ".py":
         print(f"expecting a python script, get {py_script}")
         return 1
-    log_dir = (Path("~") / ".pyscript_run" / script_name / run_time).expanduser()
+    log_dir = (Path(_LOG_DIR) / script_name / run_time).expanduser()
     log_dir.mkdir(parents=True, exist_ok=True)
     with (log_dir / py_script).open("w") as fid, open(py_script) as ori_fid:
         content = ori_fid.read()
@@ -35,18 +46,27 @@ def pyscript_run(py_script, script_args):
         if complete_proc.stderr:
             fid.write(complete_proc.stderr.decode("utf8"))
     for script_arg in script_args:
-        try:
-            file_path = Path(script_arg).expanduser()
-            if not file_path.is_file():
-                continue
-            copyfile(str(file_path), str(log_dir / file_path.name))
-        except Exception:
-            ...
+        arg_path = Path(script_arg).expanduser()
+        if arg_path.is_file():
+            copyfile(str(arg_path), str(log_dir / arg_path.name))
+        elif arg_path.is_dir() and copy_dir:
+            copy(str(arg_path), str(log_dir / arg_path.name))
     return complete_proc.returncode
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("py_script", help="the python script to run")
+    parser.add_argument(
+        "--clear", help="clear running log of given python script", action="store_true"
+    )
+    parser.add_argument(
+        "--copy-dir",
+        action="store_true",
+        help="if to copy directory given in the script aruguments",
+    )
     args, script_args = parser.parse_known_args()
-    sys.exit(pyscript_run(args.py_script, script_args))
+    if args.clear:
+        sys.exit(clear(args.py_script))
+    else:
+        sys.exit(pyscript_run(args.py_script, script_args, copy_dir=args.copy_dir))
