54e42175b160db7380edd0d94eb799e2052bc6e9
dboyliao <qmalliao@gmail.com>
2021-05-08 14:32:20 +0800

    update script

--- Python/pyscript-run.py
+++ Python/pyscript-run.py
@@ -5,6 +5,7 @@ from pathlib import Path
 import sys
 import os
 import subprocess
+from shutil import copyfile
 
 
 def pyscript_run(py_script, script_args):
@@ -19,13 +20,28 @@ def pyscript_run(py_script, script_args):
         content = ori_fid.read()
         fid.write(content)
     complete_proc = subprocess.run(
-        [sys.executable, py_script] + script_args, capture_output=True
+        [sys.executable, py_script] + script_args,
+        capture_output=True,
     )
+    if complete_proc.stdout:
+        print(complete_proc.stdout.decode("utf8"), file=sys.stdout, end="")
+    if complete_proc.stderr:
+        print(complete_proc.stderr.decode("utf8"), file=sys.stderr, end="")
+    with (log_dir / "command.txt").open("w") as fid:
+        fid.write(" ".join([py_script] + script_args) + "\n")
     with (log_dir / "log.txt").open("w") as fid:
         if complete_proc.stdout:
             fid.write(complete_proc.stdout.decode("utf8"))
         if complete_proc.stderr:
             fid.write(complete_proc.stderr.decode("utf8"))
+    for script_arg in script_args:
+        try:
+            file_path = Path(script_arg).expanduser()
+            if not file_path.is_file():
+                continue
+            copyfile(str(file_path), str(log_dir / file_path.name))
+        except Exception:
+            ...
     return complete_proc.returncode
 
 
