0a3ae08afb4d4a87ac6e54c603df4e265f73e39c
dboyliao <qmalliao@gmail.com>
2022-03-09 10:40:15 +0800

    update scripts

--- Python/cmake-find-package.py
+++ Python/cmake-find-package.py
@@ -1,30 +1,42 @@
 #!/usr/bin/env python3
 import argparse
-from subprocess import call as _call
+from subprocess import run as _run
+from tempfile import TemporaryDirectory
 
 
 def cmake_find_package(
-    package_name, language="CXX", mode="EXIST", compiler_id="GNU", print_cmake_cmd=False
+    package_names,
+    language="CXX",
+    mode="EXIST",
+    compiler_id="GNU",
+    print_cmake_cmd=False,
 ):
-    cmd = [
-        "cmake",
-        f"-DNAME={package_name}",
-        f"-DLANGUAGE={language}",
-        f"-DMODE={mode}",
-        f"-DCOMPILER_ID={compiler_id}",
-        "--find-package",
-    ]
-    if print_cmake_cmd:
-        print("Running CMake command:", " ".join(cmd))
-        print()
-    return _call(cmd)
+    ok_flags = []
+    with TemporaryDirectory(prefix="cmake_find_package_") as tmp_dir:
+        for name in package_names:
+            cmd = [
+                "cmake",
+                f"-DNAME={name}",
+                f"-DLANGUAGE={language}",
+                f"-DMODE={mode}",
+                f"-DCOMPILER_ID={compiler_id}",
+                "--find-package",
+            ]
+            if print_cmake_cmd:
+                print("Running CMake command:", " ".join(cmd))
+                print()
+            comp_proc = _run(cmd, cwd=tmp_dir, check=False, capture_output=True)
+            print(comp_proc.stdout.decode("utf8").strip())
+            ok_flags.append(comp_proc.returncode == 0)
+    ret_code = 0 if all(ok_flags) else 1
+    return ret_code
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         formatter_class=argparse.ArgumentDefaultsHelpFormatter
     )
-    parser.add_argument("package_name", help="the package name")
+    parser.add_argument("package_names", nargs="+", help="the package name")
     parser.add_argument("--language", default="CXX", help="the language id for CMake")
     parser.add_argument(
         "-m",
